1411184764ad79d7258783d4a3c80e7fa6c6d170
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2015-04-11 12:39:31 +0800

    change port

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import jssc.SerialPort;
+import jssc.SerialPortException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("hello");
-
-	}
-
+    public static void main(String[] args) {
+        SerialPort serialPort = new SerialPort("COM33");
+        try {
+            serialPort.openPort();//Open serial port
+            serialPort.setParams(9600, 8, 1, 0);//Set params.
+            byte[] buffer = serialPort.readBytes(100);//Read 10 bytes from serial port
+            serialPort.closePort();//Close serial port
+        }
+        catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
+    }
 }
--- src/SerialTest.java
+++ src/SerialTest.java
@@ -16,7 +16,7 @@ public class SerialTest implements SerialPortEventListener {
 			"/dev/tty.usbserial-A9007UX1", // Mac OS X
                         "/dev/ttyACM0", // Raspberry Pi
 			"/dev/ttyUSB0", // Linux
-			"COM27", // Windows
+			"COM33", // Windows
 	};
 	/**
 	* A BufferedReader which will be fed by a InputStreamReader 
@@ -35,7 +35,7 @@ public class SerialTest implements SerialPortEventListener {
                 // the next line is for Raspberry Pi and 
                 // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
        /// System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
-        System.setProperty("gnu.io.rxtx.SerialPorts", "COM27");
+        System.setProperty("gnu.io.rxtx.SerialPorts", "COM33");
 
 		CommPortIdentifier portId = null;
 		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
